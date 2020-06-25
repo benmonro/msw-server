0b0a514e446a6ea92784051e5ec634f642a6d785
Ben Monro <benjamin.monro@walmartlabs.com>
2020-06-25 11:06:38 -0700

    feat: gql query

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,23 +1,28 @@
-import { setupServer } from "msw/node";
-import { getEndpointsFor } from "..";
+import { setupServer } from "../";
 import nodeFetch, { Response } from 'node-fetch';
 import fetchAbsolute from 'fetch-absolute';
-
+import { request } from 'graphql-request'
 
 const fetch = fetchAbsolute(nodeFetch)("http://localhost");
-
+global.fetch = fetch;
 const data = {foo:{bar:"baz"}};
 
-const server = setupServer(...getEndpointsFor(data))
+let server = null as any;
+
+
 describe("simple rest server", () => {
-    beforeAll(() => server.listen())
-
-    // Reset any runtime request handlers we may add during the tests.
-    afterEach(() => server.resetHandlers())
 
 
-    // Disable API mocking after the tests are done.
-    afterAll(() => server.close())
+  beforeEach(() => {
+    server = setupServer(data);
+    return server.listen();
+  })
+  
+  // Reset any runtime request handlers we may add during the tests.
+  afterEach(() => server.resetHandlers())
+      // Disable API mocking after the tests are done.
+  afterAll(() => server.close())
+
   it("will create a GET", async () => {
 
     const results = await fetch("/foo");
@@ -28,9 +33,33 @@ describe("simple rest server", () => {
     const results = await fetch("/foo", {method:"POST", body:JSON.stringify({bar:"qux"})});
 
 
+    expect(await results.json()).toEqual({bar:"qux"})
 
     const results2 = await fetch("/foo");
     expect(await results2.json()).toEqual({bar:"qux"})
   })
 });
 
+describe("simple gql server", () => {
+  beforeEach(() => {
+    server = setupServer(data);
+    return server.listen();
+  })
+  
+  // Reset any runtime request handlers we may add during the tests.
+  afterEach(() => server.resetHandlers())
+      // Disable API mocking after the tests are done.
+  afterAll(() => server.close())
+  it("will create a query", async () => {
+    const query = `
+    query GetFoo {
+      foo {
+        bar
+      }
+    }`;
+
+    const results = await request("http://localhost", query);
+
+    expect(results).toEqual({bar:"baz"})
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { RequestHandlersList } from "msw/lib/types/setupWorker/glossary";
-import {rest} from 'msw';
+import {rest, graphql} from 'msw';
 import Memory from "lowdb/adapters/Memory";
 import low from 'lowdb';
 import _ from 'lodash';
-
+import {setupServer as mswSetupServer} from 'msw/node';
 
 export function getEndpointsFor(data:object): RequestHandlersList {
-    const db = low(new Memory()).setState(data);
+    const db = low(new Memory()).setState(JSON.parse(JSON.stringify(data)));
     const handlers : RequestHandlersList = [];
     _.forEach(data,(value:any, key:string) => {
 
@@ -17,9 +17,31 @@ export function getEndpointsFor(data:object): RequestHandlersList {
         handlers.push(rest.post(`/${key}`, (req,res,ctx) => {
             db.set(key, JSON.parse(req.body as string)).write(); 
             return res(ctx.json(db.get(key).value()))
+        }));
+
+        handlers.push(graphql.query(`Get${_.startCase(key)}`, (req,res,ctx) => {
+            return res(ctx.data(db.get(key).value()))
         }))
+
+        // handlers.push(graphql.mutation(_.startCase(key), (req,res,ctx) => {
+
+        // }))
+
+
     })
 
 
     return handlers;
+}
+
+export function setupServer(data: object) {
+    const server = mswSetupServer(...getEndpointsFor(data));
+
+    return {
+        listen: server.listen,
+        resetHandlers() {
+            server.resetHandlers(...getEndpointsFor(data));
+        },
+        close: server.close
+    }
 }
