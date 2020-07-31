23c75fd8ca9db8c482896c39ee1f0d35526f91dd
Ben Monro <ben.monro@gmail.com>
2020-07-31 10:15:29 -0700

    fix: updated msw, fixed browser not returning full worker

--- src/browser.ts
+++ src/browser.ts
@@ -4,7 +4,7 @@ import { getEndpointsFor } from "./getEndpoints";
 export function setupWorker(data?: object) {
   const worker = mswSetupWorker(...getEndpointsFor(data));
   return {
-    start: worker.start,
-    resetHandlers(data: object) {},
+    ...worker,
+    resetHandlers(data: object) {worker.resetHandlers(...getEndpointsFor(data))},
   };
 }
